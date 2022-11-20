2272e863df336eff06853f8c2ff2d7745c23e2b1
Alexander Waya Nguku <alex.waya@gmail.com>
2022-11-20 22:37:42 +0300

    django-allauth

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party
+    'allauth', # new
+    'allauth.account', # new
+    'allauth.socialaccount', # new
+
+    # Local
     'accounts',
 ]
 
