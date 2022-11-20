23f6e1ae7bcd77ce1d33e879973c7ea042119d4c
Alexander Waya Nguku <alex.waya@gmail.com>
2022-11-20 22:28:45 +0300

    user models declared

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
+# accounts/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # add additional fields in here
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    
+    leave_days = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.email
